8af521a5106a9f0e455f6180a7339e448a40658f
Diogenes Leonel <diogenesleonel@gmail.com>
2013-12-06 23:25:52 -0200

    - Icone que pisca

--- principal.cpp
+++ principal.cpp
@@ -15,9 +15,7 @@ Principal::Principal(QWidget *parent) :
     this->setWindowFlags(Qt::CustomizeWindowHint);
 
     serialConnect = new Serial(this);
-
-    //connect(serialConnect,SIGNAL(temperature(double)),ui->widget,SLOT(updateValues(double)));
-
+    ok=false;
 
 
 
@@ -46,14 +44,22 @@ void Principal::on_Led_toggled(bool checked)
 void Principal::on_Led_2_toggled(bool checked)
 {
 
-    if(checked){
-        ui->Led_2->setIcon(QIcon(":/icons/led-on"));
-        // Envia Comando Para Ligar Led
+    if(checked && serialConnect->getSerial()->isOpen()){
+
+        connect(&piscar,SIGNAL(timeout()),this,SLOT(piscarLed()));
+//        ui->Led_2->setIcon(QIcon(":/icons/led-on"));
+        piscar.start(500);
+        // Liga led
+            serialConnect->send(QByteArray("c"));
+
     }
     else{
-        ui->Led_2->setIcon(QIcon(":/icons/led-off"));
-        // Envia Comando Para Ligar Led
+        disconnect(&piscar,SIGNAL(timeout()),this,SLOT(piscarLed()));
 
+        ui->Led_2->setIcon(QIcon(":/icons/led-off"));
+
+        if(serialConnect->getSerial()->isOpen())
+            serialConnect->send(QByteArray("c"));
     }
 
 
@@ -70,11 +76,13 @@ void Principal::on_serial_toggled(bool checked)
 
         if(serialConnect->connectNow()){
             ui->serial->setIcon(QIcon(":/icons/serial-on"));
-            connect(serialConnect,SIGNAL(temperature(double)),ui->widget,SLOT(updateValues(double)));
+            connect(serialConnect,SIGNAL(temperature(double)),ui->
+                    widget,SLOT(updateValues(double)));
 
         }else{
             ui->serial->setIcon(QIcon(":/icons/serial-error"));
-            disconnect(serialConnect,SIGNAL(temperature(double)),ui->widget,SLOT(updateValues(double)));
+            disconnect(serialConnect,SIGNAL(temperature(double)),ui->
+                       widget,SLOT(updateValues(double)));
             ui->widget->initGraph();
 
         }
@@ -86,7 +94,8 @@ void Principal::on_serial_toggled(bool checked)
         serialConnect->closeConnection();
         ui->serial->setIcon(QIcon(":/icons/serial-off"));
 
-        disconnect(serialConnect,SIGNAL(temperature(double)),ui->widget,SLOT(updateValues(double)));
+        disconnect(serialConnect,SIGNAL(temperature(double)),ui->
+                   widget,SLOT(updateValues(double)));
         ui->widget->detachItems();
         ui->widget->initGraph();
 
@@ -94,5 +103,50 @@ void Principal::on_serial_toggled(bool checked)
 
     }
 
+
+}
+
+void Principal::on_Cooler_toggled(bool checked)
+{
+    if(checked){
+        ui->Cooler->setIcon(QIcon(":/icons/cooler-on"));
+
+        // Liga led
+        if(serialConnect->getSerial()->isOpen())
+            serialConnect->send(QByteArray("c"));
+
+    }
+    else{
+        ui->Cooler->setIcon(QIcon(":/icons/cooler-off"));
+
+        if(serialConnect->getSerial()->isOpen())
+            serialConnect->send(QByteArray("c"));
+    }
+}
+
+void Principal::on_Heater_toggled(bool checked)
+{
+    if(checked){
+        ui->Heater->setIcon(QIcon(":/icons/cooler-on"));
+
+        // Liga led
+        if(serialConnect->getSerial()->isOpen())
+            serialConnect->send(QByteArray("c"));
+
+    }
+    else{
+        ui->Heater->setIcon(QIcon(":/icons/cooler-off"));
+
+        if(serialConnect->getSerial()->isOpen())
+            serialConnect->send(QByteArray("c"));
+    }
+}
+
+void Principal::piscarLed()
+{
+
+    QString status = (ok)?"on":"off";
+    ui->Led_2->setIcon(QIcon(":/icons/led-"+status));
+    ok^=true;
 
 }
--- principal.h
+++ principal.h
@@ -2,6 +2,7 @@
 #define PRINCIPAL_H
 
 #include <QWidget>
+#include <QTimer>
 #include "plotgraph.h"
 #include "serial.h"
 
@@ -22,11 +23,17 @@ private slots:
     void on_Led_toggled(bool checked);
     void on_Led_2_toggled(bool checked);
     void on_serial_toggled(bool checked);
+    void on_Cooler_toggled(bool checked);
+    void on_Heater_toggled(bool checked);
+
+    void piscarLed();
 
 private:
     Ui::Principal *ui;
     PlotGraph *grafico;
     Serial *serialConnect;
+    QTimer piscar;
+    bool ok;
 
 };
 
